75ccea4908b4c0c7581e9d1d6c9e409ee2a292fb
sanjay.infortech@gmail.com <sanjay.infortech@gmail.com>
2018-11-26 12:05:32 +0530

    inital setup

---
+++ course/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from .models import CourseId, CourseDetails, CourseContent,Question,Progress
+
+# Register your models here.
+class CourseIdAdmin(admin.ModelAdmin):
+    search_fields = ('courseid', )
+
+
+class CourseDetailsAdmin(admin.ModelAdmin):
+
+
+    list_display = ('name', 'courseid', )
+    list_filter = ('courseid',)
+    search_fields = ('description', 'courseid', )
+
+class CourseContentAdmin(admin.ModelAdmin):
+
+
+    list_display = ('sub_title', 'courseid', )
+    list_filter = ('courseid',)
+    search_fields = ('courseid', )
+
+class QuestionAdmin(admin.ModelAdmin):
+
+
+    list_display = ('question','courseid', )
+    list_filter = ('courseid',)
+    search_fields = ('question','courseid', )
+
+class ProgressAdmin(admin.ModelAdmin):
+
+    list_display = ('user','cid', )
+    list_filter = ('cid',)
+admin.site.register(Progress, ProgressAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(CourseId, CourseIdAdmin)
+admin.site.register(CourseDetails, CourseDetailsAdmin)
+admin.site.register(CourseContent, CourseContentAdmin)
